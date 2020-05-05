51feb26475062f23d24ce563d1116abbdbc34946
Adam Casey <adam@pivotal.io>
2020-05-04 20:00:27 -0400

    Updates

---
+++ precache-manifest.60efd5bea60c12b00f52a9d5d47679c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17217da7de6edc4108bdcccf8e48e2b7",
+    "url": "/test-component/index.html"
+  },
+  {
+    "revision": "ba208a2cb6f6155260a7",
+    "url": "/test-component/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "74bd7ec2532025dcad3c",
+    "url": "/test-component/static/js/2.97e0e604.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-component/static/js/2.97e0e604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba208a2cb6f6155260a7",
+    "url": "/test-component/static/js/main.21fbf243.chunk.js"
+  },
+  {
+    "revision": "1d657b51d47673dd0045",
+    "url": "/test-component/static/js/runtime-main.e5792020.js"
+  }
+]);
